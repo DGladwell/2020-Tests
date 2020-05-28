546c580aa82861acf607b7da81bd731bf82d1f95
Daniel <gladwell1000@hotmai.com>
2020-05-28 16:17:46 +0100

    Deleted Example Cypress Tests

---
+++ cypress/integration/basetests.js
@@ -0,0 +1,24 @@
+// import {} from
+// import vars from
+
+describe('', function() {
+
+    beforeEach(function () {
+    });
+
+    it('', function(){
+    });
+
+    it('', function(){
+    });
+
+    it('', function(){
+    });
+
+    it.skip('', function(){
+    });
+
+    it.skip('', function(){
+    });
+
+})
