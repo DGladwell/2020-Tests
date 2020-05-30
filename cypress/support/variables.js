7679ab42a0070e8e974d82fb0ab4142052359465
Daniel <gladwell1000@hotmai.com>
2020-05-30 14:18:22 +0100

    Added Base Failed Login Test

---
+++ cypress/support/variables.js
@@ -0,0 +1,27 @@
+//URLs
+const homePage = 'https://www.base.com';
+const loginPage = 'https://www.base.com/account/logon.htm'
+
+//Page Messages
+const coronaVirus = 'Coronavirus (COVID-19): We are open and all orders are being delivered.'
+const incorrectDetailsMessage = '* Your Email address or Password is incorrect.'
+
+//Buttons
+const myAccountButton = '#account-options > ul > li.first > a'
+const loginButton = '#loginBtn'
+
+//Text Input Boxes
+const userNameInput = '#USR'
+const passwordInput = '#PWD'
+
+
+module.exports = {
+    coronaVirus,
+    incorrectDetailsMessage,
+    homePage,
+    loginButton,
+    loginPage,
+    myAccountButton,
+    passwordInput,
+    userNameInput
+}
